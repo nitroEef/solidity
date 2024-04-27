e93af462b8562ef58a2597648e34c6214237cc73
nitroEef <mikebodetelli@gmail.com>
2024-04-27 11:46:09 +0100

    first commit

---
+++ contracts/Dapps.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract Dapps{
+    uint storedData = 21;
+    string firstName = "opa pay";
+    bool isActive = true;
+
+    function set(){
+        // statement 
+    }
+    
+    function get(){
+        // statement 
+    }
+
+    
+    function store(){
+   uint storage
+    }
+}
